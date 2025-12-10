4ca0b77407cf1ee2a95b1c1f108e7cfcf15c5bf8
vibhuti <vibhutikariya2006@gmail.com>
2025-12-10 12:28:06 +0530

    c-task

---
+++ 1.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+int main() {
+    float celsius;
+    printf("Enter the value of celsius : ");
+    scanf("%f", &celsius);
+    float F = (9.0 / 5.0 * celsius) + 32 ;
+    
+    printf("The Temperature in Fahrient is : %f", F);
+
+
+    int bs ;
+    printf("\n Enter Your Base Salary :");
+    scanf("%d" , &bs);
+
+    int A ;
+    printf("Enter HRA :");
+    scanf("%d", &A);
+    int HRA = (bs * A/100);
+
+    int B ;
+    printf("Enter DA :");
+    scanf("%d", &B);
+    int DA = (bs * B/100);
+
+
+    int C ;
+    printf("Enter TA :");
+    scanf("%d", &C);
+    int TA = (bs * C/100);
+
+    
+    int output = bs + HRA + DA + TA;
+    printf("\n Output : Gross Salary is : %d" , output);
+    
+
+    int first;
+    printf("\n Enter the value of first angle of triangle :");
+    scanf("%d", &first);
+
+    int second;
+    printf("Enter the value of second angle of triangle :");
+    scanf("%d", &second);
+
+    int third = 180 - (first + second);
+
+    if (third > 0){
+       printf("Third angle is : %d", third);
+    }
+    else{
+        printf("Error");
+    }
+    
+    
+    
+    
+    
+    
+
+}
